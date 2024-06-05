caa9f2e48a0c7985f5f34576c49538aae95ec9cd
selinat3612 <selinatran3612@gmail.com>
2024-06-05 18:58:17 -0400

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
+import About from "./pages/aboutMe";
+import Projects from "./pages/projects";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <style>{'body {background-color: #FFF2F2; padding: 20px; padding-left:40px;}'}</style>
+      <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/aboutMe" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </Router>
+    <h1 id="heading1">HCI STUDENT</h1>
+    <h1 id="heading2">@WORCESTER POLYTECHNIC INSTITUTE <br/> B.S. IN HCI</h1>
+    <h1 id="heading">BOSTON, MA</h1>
+  </div>
   );
 }
 
---
+++ src/pages/projects.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "centre",
+                alignItems: "centre",
+                height: "100vh",
+            }}
+        >
+            <h1>My Projects</h1>
+        </div>
+    );
+};
+
+export default Projects;
