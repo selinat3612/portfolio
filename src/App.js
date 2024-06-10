e0da050ec75c39292e83f2366fbcec5e543a7d9b
selinat3612 <selinatran3612@gmail.com>
2024-06-10 19:31:32 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import Navbar from "./components/Navbar";
-import About from "./pages/aboutMe";
-import Projects from "./pages/projects";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import Navbar from './Navbar';
+import About from './Pages/About';
+import Projects from './Pages/Projects';
+import Home from './Pages/Home';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  // let Component
+  //     switch (window.location.pathname) {
+  //       case "/":
+  //         Component = Home
+  //         break
+  //       case "/about":
+  //         Component = About
+  //         break
+  //       case "/projects":
+  //         Component = Projects
+  //         break
+  //     }
   return (
     <div className="App">
-      <style>{'body {background-color: #FFF2F2; padding: 20px; padding-left:40px;}'}</style>
-      <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/aboutMe" element={<About />} />
-                <Route path="/projects" element={<Projects />} />
-            </Routes>
-        </Router>
-    <h1 id="heading1">HCI STUDENT</h1>
-    <h1 id="heading2">@WORCESTER POLYTECHNIC INSTITUTE <br/> B.S. IN HCI</h1>
-    <h1 id="heading">BOSTON, MA</h1>
+      <style>{'body {background-color: #FFFCF5; padding: 20px; padding-left:40px;}'}</style>
+      <Navbar></Navbar>
+      {/* <Component /> */}
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
   </div>
   );
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,18 +1,3 @@
-import React from "react";
-
-const Projects = () => {
-    return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "centre",
-                alignItems: "centre",
-                height: "100vh",
-            }}
-        >
-            <h1>My Projects</h1>
-        </div>
-    );
-};
-
-export default Projects;
+export default function Projects() {
+    return <h1>Projects</h1>
+}
